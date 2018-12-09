c7eae6628223a1635f8886a3f653fa8b7b205450
Sunny Singh <me@sunnysingh.io>
2018-12-09 16:39:12 -0500

    Fix gutter on small

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
+import { breakpoints } from '../config';
 
 const Container = styled.div`
   max-width: 1400px;
   margin-left: auto;
   margin-right: auto;
-  padding-left: 4rem;
-  padding-right: 4rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+
+  @media (min-width: ${breakpoints.large}px) {
+    padding-left: 4rem;
+    padding-right: 4rem;
+  }
 `;
 
 export default Container;
