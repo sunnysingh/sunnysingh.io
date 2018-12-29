90ad1b2c0a83228063a4a30b84aafd6a021556ae
Sunny Singh <me@sunnysingh.io>
2018-12-28 22:00:47 -0500

    Add footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const FooterLayout = styled.footer`
+const Container = styled.footer.attrs({
+  role: 'contentinfo',
+})`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   flex-shrink: 0;
   padding-top: 2rem;
   padding-bottom: 1rem;
+`;
+
+const Nav = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const NavLink = styled.a`
+  &:not(:last-child) {
+    margin-right: 1rem;
+  }
 `;
 
 const Copyright = styled.p`
@@ -17,8 +30,14 @@ const Copyright = styled.p`
 
 export default function Footer() {
   return (
-    <FooterLayout role="contentinfo">
-      <Copyright>© 2019 Sunny Singh</Copyright>
-    </FooterLayout>
+    <Container>
+      <Nav>
+        <NavLink href="https://buttondown.email/sunnysingh">Newsletter</NavLink>
+        <NavLink href="https://www.notion.so/sunnysingh/Web-Development-Resources-ea153ce48f7c4557862a6b8fb88d72a9">
+          Resources
+        </NavLink>
+      </Nav>
+      <Copyright>&copy; 2019 Sunny Singh</Copyright>
+    </Container>
   );
 }
