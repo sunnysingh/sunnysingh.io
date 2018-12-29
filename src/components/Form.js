821ece1b1e3ffe8a73355cce8bea0ad6631e6f78
Sunny Singh <me@sunnysingh.io>
2018-12-29 12:05:17 -0500

    Add spacing on small screens

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 const Form = styled.form`
   margin-left: auto;
   margin-right: auto;
-  max-width: 600px;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  max-width: 660px;
 `;
 
 export default Form;
