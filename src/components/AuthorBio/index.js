eb235543eb671c0eb46f63b6e513977b3a82f671
Sunny Singh <me@sunnysingh.io>
2023-11-07 18:10:47 -0500

    Update bio

--- src/components/AuthorBio/index.js
+++ src/components/AuthorBio/index.js
@@ -9,19 +9,11 @@ export default function AuthorBio() {
       <Avatar src={avatarBase64} alt="Photo of Sunny Singh" />
       <div>
         <Text>
-          <span aria-hidden="true">&#x1F44B;</span> Hello,{' '}
+          <span aria-hidden="true">&#x1F44B;</span> Hi,{' '}
           <Link to="/" title="Learn about me">
             I'm Sunny
           </Link>
-          . A software engineer at <a href="https://nomadhealth.com/">Nomad</a>
-        </Text>
-        <Text>
-          <span aria-hidden="true">🎙</span> Prefer video or audio?{' '}
-          <a href="https://sunnycommutes.fm/">Listen to my podcast</a>
-        </Text>
-        <Text>
-          <span aria-hidden="true">📨</span> Want early access?{' '}
-          <Link to="/newsletter">Subscribe to my newsletter</Link>
+          . Hope you enjoyed reading this!
         </Text>
       </div>
     </Container>
