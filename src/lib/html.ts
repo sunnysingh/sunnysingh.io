026e03fb43867c6d2ad0a65c41dcb58444e75281
Sunny Singh <me@sunnysingh.io>
2026-01-03 17:07:25 -0500

    Update docs

--- src/lib/html.ts
+++ src/lib/html.ts
@@ -1,5 +1,7 @@
 /**
- * Provides embedded language hinting for Lit-compatible tooling.
+ * Embedded language hint.
+ * Works with Prettier: https://prettier.io/docs/options#embedded-language-formatting
+ * Also VS Code: https://marketplace.visualstudio.com/items?itemName=Tobermory.es6-string-html
  * In the future, this may provide additional render capabilities.
  * IMPORTANT: This must maintain server-side compatibility.
  */
