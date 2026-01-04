26782ed3c5798ec89e2fb33d6c0591b803d2ebb4
Sunny Singh <me@sunnysingh.io>
2026-01-04 11:59:46 -0500

    Fix warning

--- src/layouts/app.ts
+++ src/layouts/app.ts
@@ -9,13 +9,12 @@ export default class AppLayout extends HTMLElement {
       <html lang="en">
         <head>
           <title>Sunny Singh</title>
-          <link rel="stylesheet" href="/node_modules/open-props/open-props.min.css"></link>
         </head>
 
         <body>
           <app-header></app-header>
           <main>
-            <content-outlet></content-outlet>
+            <output for="page"></output>
           </main>
           <app-footer></app-footer>
         </body>
